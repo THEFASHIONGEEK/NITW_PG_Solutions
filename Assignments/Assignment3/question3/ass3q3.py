48f8db282149c9f73081c6939afe12631fdf9ba3
HEMANTH KUMAR <50513357+THEFASHIONGEEK@users.noreply.github.com>
2020-03-14 14:58:51 +0530

    Add files via upload

---
+++ Assignments/Assignment3/question3/ass3q3.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+import numpy as np
+
+import csv
+
+df = pd.read_csv("/data/training/blackfriday.csv",sep=",")
+
+b = []
+
+b.append([np.round(df["Purchase"].var(),2)])
+
+b.append([np.round(df["Purchase"].std(),2)])
+
+from scipy.stats import skew
+
+b.append([np.round(skew(df["Purchase"]),2)])
+
+from scipy.stats import kurtosis
+b.append([np.round(kurtosis(df["Purchase"]),1)])
+
+with open("/code/output/output.csv", "w") as out:
+
+    writer = csv.writer(out, delimiter=",")
+
+    for i in range(0,len(b)):
+
+        writer.writerow(b[i])
