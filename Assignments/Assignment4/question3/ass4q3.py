69b66fdcc9a3cb0317dda09a2a3b28c1067b7d8e
HEMANTH KUMAR <50513357+THEFASHIONGEEK@users.noreply.github.com>
2020-03-14 14:50:18 +0530

    Add files via upload

---
+++ Assignments/Assignment4/question3/ass4q3.py
@@ -0,0 +1,29 @@
+from sklearn import datasets
+
+import numpy as np
+
+import csv
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+iris = datasets.load_iris()
+
+x = iris.data
+
+y = iris.target
+
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.model_selection import cross_val_score
+
+clf = LogisticRegression()
+
+scores = cross_val_score(clf, iris.data, iris.target, cv=5,scoring="accuracy")
+
+with open("/code/output/output.csv", "w") as out:
+
+    writer = csv.writer(out, delimiter=",")
+
+    writer.writerow([str(round(np.mean(scores),4))])
